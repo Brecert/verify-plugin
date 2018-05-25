7a217f7861baa54b5056e16ff67ca842e98a2806
Brecert <brecert@memework.org>
2018-05-25 16:43:32 -0400

    Config is now generated

--- src/main/java/io/gitlab/brecert/verifyplugin/VerifyPlugin.java
+++ src/main/java/io/gitlab/brecert/verifyplugin/VerifyPlugin.java
@@ -6,6 +6,8 @@ public final class VerifyPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+    		// Safely generate config if none.
+    		saveDefaultConfig();
         getCommand("verify").setExecutor(new VerifyCommand(this));
     }
 }
